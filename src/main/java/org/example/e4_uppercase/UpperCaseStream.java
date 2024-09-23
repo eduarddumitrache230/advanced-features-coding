16c4cf3089d139d313242454e382bbf85c2eeaaa
Eduard <edydumitrache@yahoo.com>
2024-09-23 20:58:47 +0300

    init ex 4

---
+++ src/main/java/org/example/e4_uppercase/UpperCaseStream.java
@@ -0,0 +1,32 @@
+package org.example.e4_uppercase;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+    Write a method that converts all strings in a list to their upper case using streams.
+
+    Metoda1:
+    -stream
+    -toUpperCase per fiecare element
+    -map
+
+    Metoda2:
+    -for
+    -toUpperCase?
+    -get element pe pozitita i, set cu upperCase
+
+    Metoda 3:
+
+    -extra, uitati va la functionalitatile din String
+*/
+public class UpperCaseStream {
+    public static void main(String[] args) {
+
+        List<String> text = Arrays.asList("ana","are","mere");
+
+        text.stream()
+                .map
+    }
+
+}
