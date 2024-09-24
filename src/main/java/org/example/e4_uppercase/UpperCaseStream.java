3b8539ec23bc631a71478be22f2a0ac9979f6f9a
Eduard <edydumitrache@yahoo.com>
2024-09-24 18:59:57 +0300

    final ex 4

--- src/main/java/org/example/e4_uppercase/UpperCaseStream.java
+++ src/main/java/org/example/e4_uppercase/UpperCaseStream.java
@@ -1,7 +1,11 @@
 package org.example.e4_uppercase;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Write a method that converts all strings in a list to their upper case using streams.
@@ -23,10 +27,78 @@ import java.util.List;
 public class UpperCaseStream {
     public static void main(String[] args) {
 
-        List<String> text = Arrays.asList("ana","are","mere");
+        List<String> text = new ArrayList<>(List.of("ana","are","mere"));
 
-        text.stream()
-                .map
+        displayText(text);
+
+        System.out.println("Elementele to upperCase " + upperCaseForStringB(text));
+        System.out.println("Elementele to upperCase " + upperCaseFor(text));
+        System.out.println("Elementele to upperCase " + upperCaseStream(text));
+        System.out.println("Elementele to upperCase " + "upperCaseStream");
+        System.out.println(upperCaseString(text));
+
+        /*
+        Keyoword final ne permite sa cream portiuni de cod CONSTANTE
+
+        cand o colectie de valori/elemente/obiecte este constanta, se numeste IMUTABILA
+        cand o colectie este variabila este MUTABILA
+
+        in momentul in care aveti metode void, si pasati anumite argumente din mail in aceasta, toate modificarile
+        asupra lor in metoda se vor propaga inapoi in main
+        de ce? pentru ca argumentul va crea o copie a referintei in memorie, si obiectul din argument
+        va coexista in 2 locuri
+
+        -odata in main
+        -odata in metoda
+
+        prin urmare ce prelucari se vor intampla in metoda, vor aparea si in main
+         */
+        upperCaseString(text);
+        displayText(text);
     }
+
+    private static void upperCaseString(List<String> text) {
+        text.replaceAll(it-> it.toUpperCase()); // String::toUpperCase
+    }
+
+    private static void displayText(List<String> listOfTexts){
+        for (String it: listOfTexts){
+            System.out.print(it + " ");
+        }
+        System.out.println();
+    }
+    private static String upperCaseFor(List<String> text) {
+        String textUpper = " ";
+
+        for (int i = 0; i < text.size(); i++) {
+
+            textUpper += text.get(i).toUpperCase() + " ";
+
+        }
+        return textUpper.trim();
+    }
+
+    private static String upperCaseForStringB(List<String> text) {
+
+        StringBuilder textUpper = new StringBuilder();
+
+
+        for (int i = 0; i < text.size(); i++) {
+
+            textUpper.append(text.get(i).toUpperCase()).append(" ");
+
+        }
+        return textUpper.toString().trim(); //metoda toString() se comporta ca un getter pentru StringBuilder
+        //care e un wrapper peste String
+    }
+
+    private static String upperCaseStream(List<String> text) {
+
+        return text.stream()
+                .map(it -> it.toUpperCase())
+                .collect(Collectors.joining(" "));
+    }
+
+
 
 }
